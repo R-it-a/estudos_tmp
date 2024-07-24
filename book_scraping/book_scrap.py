73d3b3861d949cf3b7f66ac8a62a7d155804de60
r-it-a <rita.cassia.barbosa.farias@gmail.com>
2024-07-23 22:16:32 -0300

    finished this porra

--- book_scraping/book_scrap.py
+++ book_scraping/book_scrap.py
@@ -1,63 +1,82 @@
-import jovian
 import requests
 from bs4 import BeautifulSoup
-import csv
 import pandas as pd
-import os
 
-#if __name__ == "__main__":
-    # boa sorte! <3
+# boa sorte! <3
 
 #beautifulsoup = parse com as infos do html
 #find procurar as infos que a gente quer. Entender o q eu quero e como eu vou pegar isso
 
-# Definir o link (linh_html = "link")
-URL = "http://books.toscrape.com/"
-#html = urlopen(URL).read()
 
-# get (conseguir os dados do html)
+def get_book_titles(doc):
+    Book_title_tags = doc.find_all('h3')
+    Book_titles = []
+    for tags in Book_title_tags:
+        Book_titles.append(tags.text)
+    return Book_titles
+
+def get_book_price(doc):
+    Book_price_tags = doc.find_all('p', class_= 'price_color')
+    Book_price = []
+    for tags in Book_price_tags:
+        Book_price.append(tags.text.replace('Â', ''))
+    return Book_price
+
+def get_stock(doc):
+    Book_stock_tag = doc.find_all('p', class_ = 'instock availability')
+    Book_stock = []
+    for stocks in Book_stock_tag:
+        Book_stock.append(stocks.text.strip())
+    return Book_stock
+
+def get_book_url(doc):
+    Book_url = []
+    for article in doc.find_all('h3'):
+        for link in article.find_all('a', href=True):
+            url = link['href']
+            links = 'http://books.toscrape.com/catalogue/' + url
+            if links not in Book_url:
+                Book_url.append(links)
+    return Book_url
+
+def get_doc(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception('Failed to load page {}'.format(response))
+    return BeautifulSoup(response.text, 'html.parser')
+
+def scrape_multiple_pages(n):
+    URL = 'https://books.toscrape.com/catalogue/page-'
+    titles, prices, stock_availability, urls = [], [], [], []
+
+    for page in range(1,n+1):
+        doc = get_doc(URL + str(page)+ '.html')
+        titles.extend(get_book_titles(doc))
+        prices.extend(get_book_price(doc))
+        stock_availability.extend(get_stock(doc))
+        urls.extend(get_book_url(doc))
+
+    book_dict1 = {
+        'TITLE': titles,
+        'PRICE': prices,
+        'STOCK_AVAILABILITY': stock_availability,
+        'URL': urls
+    }
+    return pd.DataFrame(book_dict1)
+
+
+URL = "http://books.toscrape.com/"
 response = requests.get(URL)
 page_contents = response.text
 
 #criar um file
-with open('ScrappingRitinha', 'w') as f:
+with open('Scrappingrf.html', 'w') as f:
     f.write(page_contents)
 
-#html_text = response.text
-#print(html_text)
-#print(type(html_text))
 doc = BeautifulSoup(page_contents, "html.parser")
 
 
-#print(soup)
-#print(type(soup))
-#json_response =json.dumps(html_text)
-#print(json_response)
-
-# break into lines and remove leading and trailing space on each
-
-# if html.status_code != 200:
-#    print("Error")
-#text = soup.get_text()
-#lines = (line.strip() for line in text.splitlines())
-# break multi-headlines into a line each
-#chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drop blank lines
-#text = '\n'.join(chunk for chunk in chunks if chunk)
-
-# Select all list item tags
-#list_items = soup.select('li')
-
-#print("List items: ")
-#for item in list_items:
-#    print(item)
-
-def get_book_titles(doc):
-    book_titles_tags = doc.find_add('h3')
-    book_titles = []
-    for tags in book_titles_tags:
-        book_titles.append(tags.text)
-    return book_titles
-
-
-get_book_titles(doc)
+if __name__ == "__main__":
+    df = scrape_multiple_pages(5)
+    df.to_csv('SCB.csv', index=None)
+    print("Feito caralho")
