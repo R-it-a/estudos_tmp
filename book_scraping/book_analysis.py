9026f3b4ff3f7d5b9c4f8508b0f3185ed7f20c9f
Rita Farias <rfarias@ibiunainvest.com.br>
2024-07-24 18:28:24 -0300

    Analysis

--- book_scraping/book_analysis.py
+++ book_scraping/book_analysis.py
@@ -4,8 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # Perguntas:
-#3. qual o gênero de livro com melhor avaliação na média? e qual o gênero com
-#   pior avaliação?
+
 #4. qual o total de livros disponíveis?
 #5. qual o livro mais caro? e o mais barato?
 #6. qual é o gênero mais caro em média?
@@ -17,16 +16,39 @@ from bs4 import BeautifulSoup
 #   classificador de nota dado o livro? justifique.
 
 
-df = pandas.read_csv("SCB.csv")
+df = pandas.read_csv("books.csv")
 #1. qual o total de livros?
 row_count = len(df)
 print(f"There are {row_count} books.")
 
 #2. qual o livro com mais estoque?
 
+print(df['In Stock'].max())
+print(df.nlargest(1, ['In Stock']))
 
+#3. qual o gênero de livro com melhor avaliação na média? e qual o gênero com
+#   pior avaliação?
 
-#if __name__ == "__main__":
-# boa sorte!
+rating_map = {
+    'One': 1,
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5
+}
 
+df['Star Rating'] = df['Star Rating'].map(rating_map)
 
+genre_ratings = df.groupby('Category')['Star Rating'].mean()
+
+if genre_ratings.empty:
+    print("Fodeu")
+else:
+    best_genre = genre_ratings.idxmax()
+    worst_genre = genre_ratings.idxmin()
+
+    best_genre_ratings = f'{genre_ratings[best_genre]:.2f}'
+    worst_genre_ratings = f'{genre_ratings[worst_genre]:.2f}'
+
+    print(f"The genre with best rating is: {best_genre} with {best_genre_ratings}")
+    print(f"The genre with worst rating is: {worst_genre} with {worst_genre_ratings}")
