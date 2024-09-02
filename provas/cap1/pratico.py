2fb270e4a106b0333cea5ef7c9a01a520839dba5
Giovana Morais <giovana.vmorais@gmail.com>
2024-09-02 15:25:06 -0400

    adiciona prova do capítulo 1

---
+++ provas/cap1/pratico.py
@@ -0,0 +1,40 @@
+# -*-coding: utf-8-*-
+
+def calcula_segundos(horas, minutos, segundos):
+    """
+    dado o número de horas, minutos e segundos, calcule o total de segundos
+    """
+    return
+
+
+def preco_hora(horas, salario):
+    """
+    se você ganha um salario por mes, quanto vale sua hora de trabalho?
+    """
+    return
+
+
+def preco_segundo(segundos, salario):
+    """
+    se você ganha um salario por mes, quanto vale o seu segundo de trabalho?
+    """
+    return
+
+
+if __name__ == "__main__":
+    # questao 1: quantos segundos existem em 42 minutos e 42 segundos?
+    total_segundos = calcula_segundos(horas=0, minutos=42, segundos=42)
+    print(f"em 42 minutos e 42 segundos existem {total_segundos} segundos")
+
+    # questao 2: se você ganha 5 mil por mês, trabalhando 40 horas,
+    # quanto vale sua hora?
+    preco_hora_ritinha = ...
+    print(f"o preço da minha hora trabalhada foi {preco_hora_ritinha}")
+
+    # questao 3: se você trabalha 40 horas por mês, quantos segundos você
+    # trabalha? e quanto vale o seu segundo trabalhado?
+    segundos_trabalhados = ...
+    preco_segundo_ritinha = ...
+
+    print(f"eu trabalhei 40 horas, o que significa que trabalhei {segundos_trabalhados} segundos")
+    print(f"o preço do meu segundo trabalhado foi {preco_segundo_ritinha} reais")
---
+++ provas/cap1/teorico.md
@@ -0,0 +1,30 @@
+# provinha da rita
+
+1. o que é o interpretador do python? e um script?
+2. o Python é uma linguagem fortemente tipada?
+3. qual o tipo da variável?
+	a) 2
+	b) 2.0
+	c) "2"
+	d) 2 + 2.0
+	e) 2 / 2
+	f) 2 / 2.0
+	g) 2**3
+
+4. qual o resultado? se houver erro, qual o erro? (não precisa ser específico)
+	a) 2 + "b"
+	b) 2 - 1.0
+	c) "a giovana é" + "legal"
+	d) "a giovana é" + int("10")
+	d) "a giovana é" * 2
+	e) -1 + 10.
+	f) "1" / "2"
+
+5. isso funciona? se sim, qual a saída? se não, por que não?
+	a) print(oi ritinha)
+	b) print("oi ritinha")
+	c) print("oi ritinha)
+	d) print(3 + 3 + 3)
+	e) print("ritinha" * 2)
+	f) print(3 3)
+	g) print(str(1.0))
